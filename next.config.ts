b0295bfffe2c03af9739675727070b524f04d6dd
rs691 <robstewart@my365.bellevue.edu>
2025-08-23 20:52:58 -0500

    CI/CD yml file 4

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,23 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/rob-app',
-  assetPrefix: '/rob-app',
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
