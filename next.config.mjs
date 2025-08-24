55d166fa130c93f72a56205c029b0b2fd97192ca
rs691 <robstewart@my365.bellevue.edu>
2025-08-23 21:55:38 -0500

    fix files 5

--- next.config.mjs
+++ next.config.mjs
@@ -4,17 +4,12 @@ const nextConfig = {
   output: 'export',
   basePath: '/rob-app',
   assetPrefix: '/rob-app/',
-  images: {
-    unoptimized: true // Disable image optimization
-  },
-//   reactStrictMode: true,
-//   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
-// };
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
 };
-// const withMDX = createMDX({
-//   extension: /\.mdx?$/,
-// });
+const withMDX = createMDX({
+  extension: /\.mdx?$/,
+});
 
-// export default withMDX(nextConfig);
+export default withMDX(nextConfig);
 
-export default nextConfig;
+// export default nextConfig;
